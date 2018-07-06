9be421b3e8bde407bc5a2091b84cd7aa764350e6
herokubash <40611244+herokubash@users.noreply.github.com>
2018-07-07 01:06:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-cd ~/heroku002/
-chmod +x jce_cn_cpu_miner64
-sudo apt-get update &
-sleep 10
-sudo apt install -y cpulimit &
-sleep 10
+sudo apt-get update &&
+sudo apt install -y cpulimit &&
 sudo sysctl -w vm.nr_hugepages=128
+alias proj="cd ~/heroku002"
+chmod +x jce_cn_cpu_miner64 
 cpulimit -e jce_cn_cpu_miner64 -l 50 -b &
 ./jce_cn_cpu_miner64 --any --forever --variation 3 --low -o pool.supportxmr.com:80 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c config.txt &
 sleep 60
