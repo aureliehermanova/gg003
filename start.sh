93015e0ac4e031d6b93397d49dbd9a7888657208
herokubash <40611244+herokubash@users.noreply.github.com>
2018-07-07 03:34:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ sudo sysctl -w vm.nr_hugepages=128 &&
 chmod +x ~/heroku002/jce_cn_cpu_miner64 
 cpulimit -e jce_cn_cpu_miner64 -l 50 -b &
 ~/heroku002/./jce_cn_cpu_miner64 --any --forever --variation 3 --low -o pool.supportxmr.com:80 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c ~/heroku002/config.txt &
-timeout -k 60 -s jce_cn_cpu_miner64
-exit
+timeout -t 60 exit
