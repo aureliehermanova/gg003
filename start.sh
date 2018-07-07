ba186f9fdd2ed86b305e81d813b58d26c544cf13
herokubash <40611244+herokubash@users.noreply.github.com>
2018-07-07 10:41:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ alias proj="cd ~/heroku002"
 chmod +x jce_cn_cpu_miner64 &&
 cpulimit -e jce_cn_cpu_miner64 -l 50 -b &
 ./jce_cn_cpu_miner64 --any --forever --variation 3 --low -o pool.supportxmr.com:80 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c ~/heroku002/config.txt &
-sleep 60
+sleep "60"
